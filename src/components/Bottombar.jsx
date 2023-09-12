e2bf5c85c47f1ddc5fc6ede6827f71e684103978
Jai0401 <112328542+Jai0401@users.noreply.github.com>
2023-09-12 23:31:50 +0530

    Update Bottombar.jsx

--- src/components/Bottombar.jsx
+++ src/components/Bottombar.jsx
@@ -5,7 +5,7 @@ function Bottombar(){
   return (
     <>
       <BottomNavigation sx={{background:"transparent",position:"sticky",bottom:"0",left:"45%",textAlign:"center"}}>
-        <Link href="https://github.com/aryanbhat" underline="hover">Github@AryanBhat</Link>
+        <Link href="ttps://github.com/Jai0401" underline="hover">Github@Jai0401</Link>
       </BottomNavigation>
     </>
   )
